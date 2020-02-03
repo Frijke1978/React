7783896f3bda27426d7e9999a9c5a7e6701b8cb4
FrijkeUbuntu <FrijkeUbuntu@hotmail.com>
2020-02-03 17:21:11 +0100

    toggle refactor

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -47,33 +47,38 @@ class App extends Component {
           border: '1px solid blue',
           padding: '8px',
           cursor: 'pointer'
-        };
+      };
 
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button 
-            style={style}
-            onClick= {this.togglePersonsHandler}>Toggle Persons</button>
-        { 
-          this.state.showPersons ?
-            <div>
-              <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age} />
-              <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Frijke!')}
-                changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-              <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age} />
-            </div> : null
-        }
-      </div>
-    );
+      let persons = null;
+
+      if (this.state.showPersons) {
+        persons = (
+          <div>
+            <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, 'Frijke!')}
+              changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age} />
+          </div>
+        );
+      }
+
+      return (
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p>This is really working!</p>
+          <button 
+              style={style}
+              onClick= {this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
+        </div>
+      );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
